0c60b945d64314f18c8ce08bc91ccda52df5116e
Anton Lysenko <achesco@gmail.com>
2013-10-15 20:19:40 +0300

    Переделан механизм скролла и ресайза баннерокрутилки для поддержки баннеров разной ширины (в т.ч. огромных)

--- js/f.ui.PopRollSlider.js
+++ js/f.ui.PopRollSlider.js
@@ -1,32 +1,41 @@
 /**
  * @class Слайдер
  */
-f.ui.PopRollSlider = function(options, scrollOptions) {
+f.ui.PopRollSlider = function (options, scrollOptions) {
 	f.ui.Slider.apply(this, arguments);
 	this.initLayout();
 };
 f.extend(f.ui.PopRollSlider, f.ui.Slider);
 
-f.ui.PopRollSlider.prototype.initLayout = function() {
+f.ui.PopRollSlider.prototype.initLayout = function () {
 	this.viewportWidth = this.contentStripe.width();
 	this.prevViewportWidth = this.viewportWidth;
-	this.sections = [[],[],[]];
+	this.sections = [
+		[],
+		[],
+		[]
+	];
 	this.visibleWidth = 0;
 	this.popIndex = 0;
 	var sumWidth = 0;
-	this.slideItems.each($.proxy(function(i, elem) {
+	this.slideItems.each($.proxy(function (i, elem) {
 		var item = $(elem);
 		var cwidth = item.outerWidth(true);
 		item.data("cwidth", cwidth);
-		if(i < this.currentIndex[0] ) {
+		item.data("oversized", cwidth > this.viewportWidth);
+		item.data("visibleWidth", cwidth > this.viewportWidth ? 0 : cwidth);
+		if (item.data("oversized")) {
+			item.hide();
+		}
+		if (i < this.currentIndex[0]) {
 			item.hide();
 			this.sections[0].push(item);
-		}
-		else {
-			sumWidth += cwidth;
-			if(sumWidth < this.viewportWidth) {
+		} else {
+			sumWidth = sumWidth + item.data("visibleWidth");
+
+			if (sumWidth < this.viewportWidth) {
 				this.sections[1].push(item);
-				this.visibleWidth += cwidth;
+				this.visibleWidth = this.visibleWidth + item.data("visibleWidth");
 			} else {
 				this.sections[2].push(item);
 				item.hide();
@@ -37,100 +46,160 @@ f.ui.PopRollSlider.prototype.initLayout = function() {
 	$(window).resize($.proxy(this.onResize, this));
 }
 
-f.ui.PopRollSlider.prototype.beforeScroll = function(toIndex) {
+f.ui.PopRollSlider.prototype.beforeScroll = function (toIndex) {
 	return this.sections[this.currentIndex[0] < toIndex ? 2 : 0].length > 0;
 };
 
-f.ui.PopRollSlider.prototype.performScroll = function(toIndex) {
-	if(this.currentIndex[0] < toIndex) {
-		var item = this.sections[1].shift();
-		this.visibleWidth -= item.data("cwidth");
-		this.sections[0].push( item.hide(this.options.duration) );
-		item = this.sections[2].shift();
-		this.visibleWidth += item.data("cwidth");
-		this.sections[1].push( item.show(this.options.duration) );
+f.ui.PopRollSlider.prototype.performScroll = function (toIndex) {
+	var
+		itemToMove,
+		itemsToShow,
+		itemsToHide;
+
+	if (this.currentIndex[0] < toIndex) {
+		// двигаем ленту влево
+
+		// перемещаем невидимые большие баннеры из правой секцмм в среднюю, чтобы первый правый баннер можно было показывать
+		while (this.sections[2].length && this.sections[2][0].data("oversized")) {
+			this.sections[1].push(this.sections[2].shift());
+		}
+
+		// помечаем первый правый баннер для показа, перемещаем в среднюю секцию
+		itemsToShow = this.sections[2][0];
+		this.visibleWidth += itemsToShow.data("cwidth");
+		this.sections[1].push(this.sections[2].shift());
+
+		// пока все средние баннеры не помещаются, помечаем первый средний для скрытия, и перемещаем его в левую секцию
+		itemsToHide = this.sections[1][0];
+		while (this.visibleWidth > this.viewportWidth) {
+			itemToMove = this.sections[1][0];
+			itemsToHide = itemsToHide.add(itemToMove);
+			this.visibleWidth -= itemToMove.data("visibleWidth");
+			this.sections[0].push(this.sections[1].shift());
+		}
+
+		// пока первый правый баннер помещается к средним, помечаем его для показа, и перемещаем в среднюю секцию
+		itemToMove = this.sections[2][0];
+		while (this.sections[2].length && (this.visibleWidth + itemToMove.data("visibleWidth")) <= this.viewportWidth) {
+			if (!itemToMove.data("oversized")) {
+				itemsToShow = itemsToShow.add(itemToMove);
+			}
+			this.visibleWidth += itemToMove.data("visibleWidth");
+			this.sections[1].push(this.sections[2].shift());
+			itemToMove = this.sections[2][0];
+		}
+
+	} else {
+		// двигаем ленту вправо
+
+		// перемещаем невидимые большие баннеры из левой секции в среднюю, чтобы последний левый баннер можно было показывать
+		while (this.sections[0].length && this.sections[0][this.sections[0].length - 1].data("oversized")) {
+			this.sections[1].unshift(this.sections[0].pop());
+		}
+
+		// помечаем последний левый баннер для показа, перемещаем в среднюю секцию
+		itemsToShow = this.sections[0][this.sections[0].length - 1];
+		this.visibleWidth += itemsToShow.data("visibleWidth");
+		this.sections[1].unshift(this.sections[0].pop());
+
+		// пока все средние баннеры не помещаются, помечаем последний средний для скрытия, и перемещаем его в правую секцию
+		itemsToHide = this.sections[1][this.sections[1].length - 1];
+		while (this.visibleWidth > this.viewportWidth) {
+			itemToMove = this.sections[1][this.sections[1].length - 1];
+			itemsToHide = itemsToHide.add(itemToMove);
+			this.visibleWidth -= itemToMove.data("visibleWidth");
+			this.sections[2].unshift(this.sections[1].pop());
+		}
+
+		// пока последний левый баннер помещается к средним, помечаем его для показа, и перемещаем в среднюю секцию
+		itemToMove = this.sections[0][this.sections[0].length - 1];
+		while (this.sections[0].length && (this.visibleWidth + itemToMove.data("visibleWidth") <= this.viewportWidth)) {
+			if (!itemToMove.data("oversized")) {
+				itemsToShow = itemsToShow.add(itemToMove);
+			}
+			this.visibleWidth += itemToMove.data("visibleWidth");
+			this.sections[1].unshift(this.sections[0].pop());
+			itemToMove = this.sections[0][this.sections[0].length - 1];
+		}
+
 	}
-	else {
-		var item = this.sections[1].pop();
-		this.visibleWidth -= item.data("cwidth");
-		this.sections[2].unshift( item.hide(this.options.duration) );
-		item = this.sections[0].pop();
-		this.visibleWidth += item.data("cwidth");
-		this.sections[1].unshift( item.show(this.options.duration) );
-	}
+
+	itemsToHide.hide(this.options.duration);
+	itemsToShow.show(this.options.duration);
+
 };
 
-f.ui.PopRollSlider.prototype.afterScroll = function(toIndex) {
+f.ui.PopRollSlider.prototype.afterScroll = function (toIndex) {
 	this.currentIndex[0] = this.sections[0].length;
-	this.scrollPrevControl[this.sections[0].length ? "removeClass" : "addClass"](this.options.hiddenClassName);
-	this.scrollNextControl[this.sections[2].length ? "removeClass" : "addClass"](this.options.hiddenClassName);
+	var section0Length = this.sections[0].length;
+	$.each(this.sections[0], function (i, item) {
+		if (item.data("oversized")) {
+			section0Length--;
+		}
+	});
+	var section2Length = this.sections[2].length;
+	$.each(this.sections[2], function (i, item) {
+		if (item.data("oversized")) {
+			section2Length--;
+		}
+	});
+	this.scrollPrevControl[section0Length ? "removeClass" : "addClass"](this.options.hiddenClassName);
+	this.scrollNextControl[section2Length ? "removeClass" : "addClass"](this.options.hiddenClassName);
 };
 
-f.ui.PopRollSlider.prototype.onResize = function() {
+f.ui.PopRollSlider.prototype.onResize = function () {
 	var viewportWidth = this.contentStripe.width();
-	if(viewportWidth == this.prevViewportWidth)
+	if (viewportWidth == this.prevViewportWidth)
 		return;
 
-	if(viewportWidth > this.prevViewportWidth) {
-		if(this.sections[1].length > this.lastIndex)
-			return;
+	// проходимся по баннерам, ставим/снимаем флаг большого баннера, большие скрываем
+	this.slideItems.each($.proxy(function (i, elem) {
+		var item = $(elem);
+		item.data("oversized", item.data("cwidth") > viewportWidth);
+		item.data("visibleWidth", item.data("oversized") ? 0 : item.data("cwidth"));
 
-		if(this.sections[2].length) {
-			var sumWidth = this.visibleWidth;
-			var idx = -1;
-			$.each(this.sections[2], function(i, item) {
-				sumWidth += item.data("cwidth");
-				if(sumWidth > viewportWidth)
-					return false;
-				idx = i;
-			});
-			if(idx >=0) {
-				for(var i = 0; i <= idx; i++) {
-					var item = this.sections[2].shift();
-					this.visibleWidth += item.data("cwidth");
-					this.sections[1].push( item.show(this.options.duration) );
-				}
-			}
+		if (item.data("oversized")) {
+			item.hide(this.options.duration);
 		}
+	}, this));
 
-		if(!this.sections[2].length && this.sections[0].length) {
-			var sumWidth = this.visibleWidth;
-			var idx = -1;
-			this.sections[0].reverse();
-			$.each(this.sections[0], function(i, item) {
-				sumWidth += item.data("cwidth");
-				if(sumWidth > viewportWidth)
-					return false;
-				idx = i;
-			});
-			this.sections[0].reverse();
-			if(idx >=0) {
-				for(var i = 0; i <= idx; i++) {
-					var item = this.sections[0].pop();
-					this.visibleWidth += item.data("cwidth");
-					this.sections[1].unshift( item.show(this.options.duration) );
+	var itemToMove;
+
+	// обнуляем ширину видимых баннеров, а также среднюю и правую секцию, т.к. они будут наполнены заново ниже
+	this.visibleWidth = 0;
+	this.sections[1] = [];
+	this.sections[2] = [];
+
+	// пока каждый баннер (после левых) помещается, показываем его и добавляем к средней секции
+	this.slideItems.each($.proxy(function (i, elem) {
+		if (i >= this.sections[0].length) {
+			var item = $(elem);
+
+			if (this.visibleWidth + item.data("visibleWidth") < viewportWidth) {
+				this.sections[1].push(item);
+				if (!item.data("oversized")) {
+					item.show(this.options.duration);
 				}
+				this.visibleWidth += item.data("visibleWidth");
+			} else {
+				// если баннеры уже не помещаются, скрываем их и добавляем к правой секции
+				this.sections[2].push(item);
+				item.hide(this.options.duration);
 			}
 		}
-	}
-	else {
-		var sumWidth = 0;
-		var idx = -1;
-		$.each(this.sections[1], function(i, item) {
-			sumWidth += item.data("cwidth");
-			if(sumWidth > viewportWidth) {
-				idx = i;
-				return false;
-			}
-		});
-		if(idx >=0) {
-			for(var i = this.sections[1].length - 1; i >= idx; i--) {
-				var item = this.sections[1].pop();
-				this.visibleWidth -= item.data("cwidth");
-				this.sections[2].unshift( item.hide() );
-			}
+	}, this));
+
+	// пока последний левый баннер помещается к средним, показываем его (если он не большой), и перемещаем в среднюю секцию
+	itemToMove = this.sections[0][this.sections[0].length - 1];
+	while (this.sections[0].length && (this.visibleWidth + itemToMove.data("visibleWidth") <= viewportWidth)) {
+		if (!itemToMove.data("oversized")) {
+			itemToMove.show(this.options.duration);
 		}
+		this.visibleWidth += itemToMove.data("visibleWidth");
+		this.sections[1].unshift(this.sections[0].pop());
+		itemToMove = this.sections[0][this.sections[0].length - 1];
 	}
+
 	this.afterScroll();
 	this.viewportWidth = viewportWidth;
 	this.prevViewportWidth = viewportWidth;
