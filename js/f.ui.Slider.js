2648d507e7a7d33cd5ff726fb5c8ad40529272d6
Anton Lysenko <achesco@gmail.com>
2013-11-03 18:41:12 +0200

    slight code formatting

--- js/f.ui.Slider.js
+++ js/f.ui.Slider.js
@@ -1,28 +1,28 @@
 /**
- * Абстрактный базовый суперкласс для конкретных имплементаций слайдера. 
+ * Абстрактный базовый суперкласс для конкретных имплементаций слайдера.
  */
-f.ui.Slider = function(options, scrollOptions) {
+f.ui.Slider = function (options, scrollOptions) {
 
-	this.options = $.extend({}, this.options, this.defaultOptions, (options || {}) );
-	this.scrollOptions = $.extend({}, this.scrollOptions, this.defaultScrollOptions, (scrollOptions || {}) );
-    
-    this.slideItems = $(this.options.slideItems);
-    this.contentStripe = $(this.options.contentStripe);
+	this.options = $.extend({}, this.options, this.defaultOptions, (options || {}));
+	this.scrollOptions = $.extend({}, this.scrollOptions, this.defaultScrollOptions, (scrollOptions || {}));
 
-    this.currentIndex = [0];
-    this.lastIndex = this.slideItems.size() - 1;
+	this.slideItems = $(this.options.slideItems);
+	this.contentStripe = $(this.options.contentStripe);
 
-    // init prev-next controlls and show next (optional)
-    this.scrollPrevControl = $(this.options.scrollPrevControl);
-    this.scrollNextControl = $(this.options.scrollNextControl);
-    
-    // init previews controls (optional)
-    this.previewItems = $(this.options.previewItems);
-    
-    this.initUserEvents();
-    
-    // init scroll on idle (optional) 
-    this.options.idleScroll && this.initIdleScroll();
+	this.currentIndex = [0];
+	this.lastIndex = this.slideItems.size() - 1;
+
+	// init prev-next controlls and show next (optional)
+	this.scrollPrevControl = $(this.options.scrollPrevControl);
+	this.scrollNextControl = $(this.options.scrollNextControl);
+
+	// init previews controls (optional)
+	this.previewItems = $(this.options.previewItems);
+
+	this.initUserEvents();
+
+	// init scroll on idle (optional)
+	this.options.idleScroll && this.initIdleScroll();
 };
 f.ui.Slider.prototype = {
 	/**
@@ -33,27 +33,18 @@ f.ui.Slider.prototype = {
 		, slideItems: null // String|Element|jQuery
 		, scrollPrevControl: null // String|Element|jQuery
 		, scrollNextControl: null // String|Element|jQuery
-		, previewItems: null
-		, duration: 1000
-		, idleScroll: false
-		, idleScrollInterval: 4000
-		, idleScrollIntroOnly: true
-		, onBeforeScroll: null
-		, onAfterScroll: null
-		, hiddenClassName: 'g-hidden'
-		, selectedClassName: 'selected'
-		, previewSelectedClassName: "selected"
+		, previewItems: null, duration: 1000, idleScroll: false, idleScrollInterval: 4000, idleScrollIntroOnly: true, onBeforeScroll: null, onAfterScroll: null, hiddenClassName: 'g-hidden', selectedClassName: 'selected', previewSelectedClassName: "selected"
 	},
-	
+
 	/**
 	 * Опции конкретной имплементации слайдера.
 	 */
 	defaultScrollOptions: {},
-	
+
 	/**
 	 * Базовая инициализация пользовательских событий.
 	 */
-	initUserEvents: function() {
+	initUserEvents: function () {
 		this.scrollPrevControl.click($.proxy(this.onScrollPrev, this));
 		this.scrollNextControl.click($.proxy(this.onScrollNext, this));
 		this.previewItems.click($.proxy(this.onPreviewClick, this));
@@ -62,42 +53,42 @@ f.ui.Slider.prototype = {
 	/**
 	 * Запустить периодический "самоскрол".
 	 */
-	initIdleScroll: function() {
+	initIdleScroll: function () {
 		this.idleSlideTimeout = null;
 		this.setIdleSlideTimeout();
 		this.contentStripe
 			.mouseenter($.proxy(this.clearIdleSlideTimeout, this))
 			.mouseleave($.proxy(this.setIdleSlideTimeout, this));
 
-		if(this.options.idleScrollIntroOnly) {
-			this.previewItems.one('click', $.proxy(function() {
+		if (this.options.idleScrollIntroOnly) {
+			this.previewItems.one('click', $.proxy(function () {
 				this.contentStripe
 					.unbind('mouseenter', this.clearIdleSlideTimeout)
 					.unbind('mouseleave', this.setIdleSlideTimeout);
 			}, this));
 		}
 	},
-	
+
 	/**
 	 * Запустить таймаут "самоскрола".
 	 */
-	setIdleSlideTimeout: function() {
+	setIdleSlideTimeout: function () {
 		this.idleSlideTimeout = setTimeout($.proxy(this.idleSlide, this), this.options.idleScrollInterval);
 	},
 
 	/**
 	 * Очистить таймаут "самоскрола".
 	 */
-	clearIdleSlideTimeout: function() {
+	clearIdleSlideTimeout: function () {
 		clearTimeout(this.idleSlideTimeout);
 	},
 
 	/**
 	 * Выполнить "самоскрол".
 	 */
-	idleSlide: function() {
+	idleSlide: function () {
 		var toIndex = this.currentIndex[0];
-		if(++toIndex > this.lastIndex)
+		if (++toIndex > this.lastIndex)
 			toIndex = 0;
 		this.scroll(toIndex);
 		this.setIdleSlideTimeout();
@@ -107,90 +98,90 @@ f.ui.Slider.prototype = {
 	 * Возвращает первый активный слайд.
 	 * @returns {jQuery} Слайд
 	 */
-	getCurrentItem: function() {
+	getCurrentItem: function () {
 		return this.getItem(this.currentIndex[0]);
 	},
-	
+
 	/**
 	 * Возвращает слайд по индексу.
 	 * @returns {jQuery} Слайд
 	 */
-	getItem: function(idx) {
+	getItem: function (idx) {
 		return $(this.slideItems.get(idx));
 	},
 
 	/**
 	 * Клик по стрелке "назад"
 	 */
-	onScrollPrev: function() {
+	onScrollPrev: function () {
 		this.scroll(null, -1);
 	},
 
 	/**
 	 * Клик по стрелке "вперед"
 	 */
-	onScrollNext: function() {
+	onScrollNext: function () {
 		this.scroll(null, 1);
 	},
-	
+
 	/**
 	 * Клик по конкретной превьюшке
 	 */
-	onPreviewClick: function(event) {
+	onPreviewClick: function (event) {
 		this.scroll(this.previewItems.index($(event.target)));
 	},
-	
+
 	/**
 	 * Выполнить скрол
 	 */
-	scroll: function(toIndex, delta) {
+	scroll: function (toIndex, delta) {
 		this.clearIdleSlideTimeout();
 		toIndex = toIndex === null ? this.currentIndex[0] + delta : toIndex;
 
 		var perform = true;
-		if(this.beforeScroll(toIndex)) {
-			if(typeof this.options.onBeforeScroll == "function") {
+		if (this.beforeScroll(toIndex)) {
+			if (typeof this.options.onBeforeScroll == "function") {
 				perform = this.options.onBeforeScroll();
 			}
 
-			if(perform) {
+			if (perform) {
 				this.performScroll(toIndex);
 				this.afterScroll(toIndex);
 			}
 
 		}
 	},
-	
+
 	/**
-	 * Действия перед выполнением скрола. Выполняет проверки границ для 
+	 * Действия перед выполнением скрола. Выполняет проверки границ для
 	 * слайдера одиночных слайдов.
 	 * @returns {Bool} выполнить ли скрол.
 	 */
-	beforeScroll: function(toIndex) {
-		return (toIndex != this.currentIndex[0] && toIndex >=0 && toIndex <= this.lastIndex);
+	beforeScroll: function (toIndex) {
+		return (toIndex != this.currentIndex[0] && toIndex >= 0 && toIndex <= this.lastIndex);
 	},
-	
+
 	/**
 	 * Действия после выполнением скрола.
 	 */
-	afterScroll: function(toIndex) {
+	afterScroll: function (toIndex) {
 		this.currentIndex[0] = toIndex;
 		this.scrollPrevControl[this.currentIndex <= 0 ? "addClass" : "removeClass"](this.options.hiddenClassName);
 		this.scrollNextControl[this.currentIndex >= this.lastIndex ? "addClass" : "removeClass"](this.options.hiddenClassName);
 	},
-	
+
 	/**
 	 * Действия после выполнения анимации скрола.
 	 */
-	afterPerformScroll: function(toIndex) {
+	afterPerformScroll: function (toIndex) {
 
 	},
-	
+
 	/**
 	 * Метод выполняющий непосредственную работу. Реализация зависит
 	 * от имплементации.
 	 */
-	performScroll: function() {
-		
+	performScroll: function () {
+
 	}
 };
